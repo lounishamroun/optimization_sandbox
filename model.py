ed5c1174a4fc5fb9ec3914cf28b1f546965b0ab6
Lounis <lounis.hamroun@gmail.com>
2026-02-18 16:36:10 +0100

    feat:inference test using Resnet50

--- model.py
+++ model.py
@@ -1,5 +1,36 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-from keras import keras_nlp
+# Load model directly
+from transformers import AutoImageProcessor, AutoModelForImageClassification
+import torch
+from torchvision.transforms import v2
+from PIL import Image
+import inspect
 
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.1-8B")
+
+if torch.cuda.is_available() == True:
+    device="cuda"
+device="cpu"
+    
+    
+processor = AutoImageProcessor.from_pretrained("microsoft/resnet-50",device_map=device)
+model = AutoModelForImageClassification.from_pretrained("microsoft/resnet-50")
+
+
+raw_image=Image.open("data/test_img.png",mode='r').convert("RGB") #convert to PIL
+
+transforms=v2.Compose([
+    v2.ToImage(),
+    v2.Resize(size=(224,224)),
+    v2.ConvertImageDtype(torch.float64)
+    ]
+)
+
+processed_image=transforms(raw_image)
+
+print(type(processed_image))
+
+input=processor(images=processed_image,return_tensors="pt",device=device) #torch.cuda.FloatTensor
+
+with torch.no_grad():
+    output=model(**input)
+
+print(output)
