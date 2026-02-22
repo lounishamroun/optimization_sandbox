bb3181bd91651b4109f9f215cbe1213d8632c993
Lounis <lounis.hamroun@gmail.com>
2026-02-22 23:00:46 +0100

    feat: Computing average time (per iteration)

--- model.py
+++ model.py
@@ -7,11 +7,14 @@ import inspect
 import re
 
 if torch.cuda.is_available() == True:
-    device="cuda"
+    device="cuda:0"
 else:
     device="cpu"
     
+print(f"Current device is {device}")
+    
 USE_FP16=True
+BATCH=30
 
 processor = AutoImageProcessor.from_pretrained("microsoft/resnet-50")
 model = AutoModelForImageClassification.from_pretrained("microsoft/resnet-50",device_map=device)
@@ -19,35 +22,44 @@ model = AutoModelForImageClassification.from_pretrained("microsoft/resnet-50",de
 if USE_FP16==True:
     model=model.half()
 
+assert torch.device(model.device)==torch.device(device), f"Current device = {device} | model device = {model.device} "
     
 img = Image.open("data/test_img.png").convert("RGB")
 
 
 '''Benchmarking Function'''
 
-def bench(batch):
+def bench(batch,inputs,iterations):
     inputs=processor(images=[img]*batch,return_tensors="pt")
-    inputs={k:v.to_device(device=device,non_blocking=True) for k,v in inputs.items()}
+    inputs.to(device=device)
+    inputs.to(torch.float16)
 
-        
+    #warmup
     with torch.inference_mode():
         for _ in range(30):
             _=model(**inputs)
     torch.cuda.synchronize()
     
     iters=200
-    start=torch.cuda.Event(elapsed_time=True)
-    end=torch.cuda.Event(elapsed_time=True)
+    start=torch.cuda.Event(enable_timing=True)
+    end=torch.cuda.Event(enable_timing=True)
     
     with torch.inference_mode():
         if USE_FP16==True:
             inputs["pixel_values"]=inputs["pixel_values"].half() #reducing precision of the actual image tensor
+        
         start.record()
         for _ in range(iters):
             _=model(**inputs)
         end.record()
-    torch.cuda.synchronize()   
+    torch.cuda.synchronize()
+    return (start.elapsed_time(end))/iterations
+
+
+processed_inputs=processor(images=img,return_tensors="pt",device=device)
 
 
 
-input=processor(images=img,return_tensors="pt",device=device) #torch.cuda.FloatTensor
+elapsed_time=bench(BATCH,iterations=200,inputs=processed_inputs)
+
+print(f'Elapsed time is the following : {elapsed_time:>3}ms')
--- tests/micro_tests.py
+++ tests/micro_tests.py
@@ -4,14 +4,15 @@ ONLY USED IN ORDER TO ISOLATE FUNCTION SNIPPETS
 
 import torch
 
+#in inference optimization, we need to launch a first warmup to avoid measuring useless stuff
+
 start=torch.cuda.Event(enable_timing=True)
-stop=torch.cuda.Event(enable_timing=True)
+end=torch.cuda.Event(enable_timing=True)
 
 start.record()
-torch.matmul(torch.randn(size=(10,5)),torch.randn(size=(5,10)))
-stop.record()
+torch.matmul(torch.randn(size=(20,20)),torch.randn(size=(20,20)))
+end.record()
 
-torch.cuda.synchronize()
+print(start.elapsed_time(end))
 
-print(start.elapsed_time(stop))
-
+print(torch.cuda.get_device_name())
