80a319bb45756f4cf8650bb9148996c2f2809556
Lounis <lounis.hamroun@gmail.com>
2026-02-26 19:05:11 +0100

    feat: Added a reusable boilerplate to time a target function execution on GPU

--- boilerplates/measuring_execution_time.py
+++ boilerplates/measuring_execution_time.py
@@ -1,3 +1,20 @@
 import torch
 
+def bench(target_fn,warmup=20):
 
+    torch.cuda.synchronize()
+    start=torch.cuda.Event(enable_timing=True)
+    end=torch.cuda.Event(enable_timing=True)
+    
+    #warmup bench
+    for _ in warmup:
+        target_fn()
+    
+    #real bench
+    start.record()
+    target_fn()
+    end.record()
+    torch.cuda.synchronize()
+    
+    time_ms=start.elapsed_time(end) #elapsed time
+    return time_ms
