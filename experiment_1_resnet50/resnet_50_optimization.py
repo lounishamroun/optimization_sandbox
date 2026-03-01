12ed7808ac8a995415534b59791b3e1f996dd1a9
Lounis <lounis.hamroun@gmail.com>
2026-03-01 14:57:56 +0100

    fix:Adding 'no_grad'context manager, since compile tuning only supports frozen models

--- experiment_1_resnet50/resnet_50_optimization.py
+++ experiment_1_resnet50/resnet_50_optimization.py
@@ -21,8 +21,9 @@ model = AutoModelForImageClassification.from_pretrained("microsoft/resnet-50").t
  
 if USE_FP16==True:
     model=model.half()
-    
-compiled_model=torch.compile(model,mode="max-autotune",fullgraph=True) #will raise an error in case of graph break
+
+with torch.no_grad():
+    compiled_model=torch.compile(model,mode="max-autotune",fullgraph=True) #will raise an error in case of graph break
 
 
 
