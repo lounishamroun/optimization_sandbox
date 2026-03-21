e1fcd3995e593aff04a832e3569695d03671c5b1
Lounis <lounis.hamroun@gmail.com>
2026-03-21 21:39:00 +0100

    feat:cleaning

--- playground.py
+++ playground.py
@@ -1,17 +1,1 @@
 import torch
-from boilerplates.measuring_execution_time import bench 
-torch._logging.set_logs(graph_breaks=True)
-
-def bar(a, b):
-    x = a / (torch.abs(a) + 1)
-    if b.sum() < 0:
-        b = b * -1
-    return x * b
-
-
-opt_bar = torch.compile(bar)
-inp1 = torch.ones(10)
-inp2 = torch.ones(10)
-
-opt_bar(inp1, inp2)
-opt_bar(inp1, -inp2)
