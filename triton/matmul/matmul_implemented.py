6e2f4a710dfead3d26683328e7fa978513304f80
Lounis <lounis.hamroun@gmail.com>
2026-04-08 23:33:51 +0200

    feat: initial triton code

--- triton/matmul/matmul_implemented.py
+++ triton/matmul/matmul_implemented.py
@@ -3,16 +3,8 @@ import triton
 '''MULTIPLY A=(M,K) | B=(K,M) => A@B'''
 
 '''Attempt 1'''
-@triton_jit
-for m in range(M,SIZE_M):#program 1
-    for k in range(K,SIZE_K):#program 2
-        acc=zero([m;m:BLOCKSIZE_M],[k;k:BLOCKSIZE_K]) #parallel tile computation, has to be reset at 0
-        for l in range(l,SIZE_l):
-            A=[m;m:BLOCKSIZE_M]
-            B=[k;k:BLOCKSIZE_K]
-            k=zero[l;m:BLOCKSIZE_l]
-    acc=A@B
-    k=acc
+#pid*start_ptr_m + tl.arange(0,BLOCKSIZE_M) % M => in case m is not a multiple of blocksize
+
 
     
 
