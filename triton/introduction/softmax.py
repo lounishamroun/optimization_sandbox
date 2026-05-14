e1da0630e873ed823ea957a8269b47e264ff7b37
Lounis <lounis.hamroun@gmail.com>
2026-05-14 15:35:39 +0200

    docs: Add explanation on the benchmarking part

--- triton/introduction/softmax.py
+++ triton/introduction/softmax.py
@@ -239,3 +239,39 @@ def softmax(x):
     # Create a number of persistent programs.
     kernel[(num_programs, 1, 1)](y, x, x.stride(0), y.stride(0), n_rows, n_cols, BLOCK_SIZE, num_stages)
     return y
+
+
+@triton.testing.perf_report(
+    triton.testing.Benchmark(
+        x_names=['N'],  # argument names to use as an x-axis for the plot
+        x_vals=[128 * i for i in range(2, 100)],  # different possible values for `x_name`
+        line_arg='provider',  # argument name whose value corresponds to a different line in the plot
+        line_vals=['triton', 'torch', 'naive_softmax'],  # possible values for `line_arg``
+        line_names=["Triton", "Torch", "Naive Softmax"],  # label name for the lines
+        styles=[('blue', '-'), ('green', '-'), ('red', '-')],  # line styles
+        ylabel="GB/s",  # label name for the y-axis
+        plot_name="softmax-performance",  # name for the plot. Used also as a file name for saving the plot.
+        args={'M': 4096},  # values for function arguments not in `x_names` and `y_name`
+    ))
+def benchmark(M, N, provider):
+    x = torch.randn(M, N, device=DEVICE, dtype=torch.float32)
+    stream = getattr(torch, DEVICE.type).Stream()
+    getattr(torch, DEVICE.type).set_stream(stream)
+    if provider == 'torch':
+        ms = triton.testing.do_bench(lambda: torch.softmax(x, axis=-1)) #Torch native Softmax function
+        #'do_bench' needs a calable object that's why use 'lambda' instead of a function call to 'softmax()'.
+    if provider == 'triton':
+        ms = triton.testing.do_bench(lambda: softmax(x)) #Our Kernel
+    if provider == 'naive_softmax':
+        ms = triton.testing.do_bench(lambda: naive_softmax(x))
+    gbps = lambda ms: 2 * x.numel() * x.element_size() * 1e-9 / (ms * 1e-3)
+    #2 * (n elements of the matrix * size of each elements in bytes)* 1e-9 / (duration in ms * 1e-3) 
+    '''
+    We put a '2*' simply because there's 2 memory operations:
+        1/ Read input tensor x
+        2/ Write output tensor y
+    '''
+    return gbps(ms)
+
+
+benchmark.run(show_plots=True, print_data=True)
